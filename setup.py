2a5ec13438cecdbcca357454dd926b1fbaa68e8d
Curtis Maloney <curtis@tinbrain.net>
2017-03-05 10:39:25 +1100

    Extend gitignore

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 
 setup(
     name='graaf',
-    version='0.1.0',
+    version='0.2.0',
     description='Simple static site generator',
     author='Curtis Maloney',
     author_email='curtis@tinbrain.net',
     url='https://github.com/funkybob/graaf',
-    packages=['graaf',],
+    packages=['graaf'],
     install_requires=[
         'stencil-template',
         'Markdown',
