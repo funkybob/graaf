8dcf316bf5438918b03ade757717d0e4680ddef8
Curtis Maloney <curtis@tinbrain.net>
2017-03-06 17:11:15 +1100

    Support config argumnts to scss. Fix file extension

--- graaf/scss.py
+++ graaf/scss.py
@@ -10,16 +10,24 @@ from .base import Generator
 class SassGenerator(Generator):
     extensions = ['.scss', '.sass']
 
+    def __init__(self, include_paths=None, include_source=True, output_style='nested'):
+        self.include_paths = include_paths or []
+        self.include_source = include_source
+        self.output_style = output_style
+
     def can_process(self, filename):
         return super(SassGenerator, self).can_process(filename) and not filename.startswith('_')
 
     def process(self, root, dest_dir, filename, processor):
         basename, ext = os.path.splitext(filename)
 
-        compiler = Compiler(search_path=[processor.srcdir])
+        search_path = self.include_paths
+        if self.include_source:
+            search_path.append(processor.srcdir)
+        compiler = Compiler(search_path=search_path, output_style=self.output_style)
 
         with open(os.path.join(root, filename)) as fin:
-            with open(os.path.join(dest_dir, filename), 'w') as fout:
+            with open(os.path.join(dest_dir, basename + '.css'), 'w') as fout:
                 fout.write(compiler.compile_string(fin.read()))
 
         return True
